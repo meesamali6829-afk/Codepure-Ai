3acc0b218297043c356d07bc407aec7ac4a0ed5b
meesamali6829-afk <meesamali6829@gmail.com>
2026-05-16 15:28:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -745,7 +745,7 @@ def process_code():
         for attempt in range(5):
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                    model="gemini-1.5-flash",
                     contents=user_prompt,
                     config=types.GenerateContentConfig(
                         system_instruction=system_prompt,
